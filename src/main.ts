a626dc671a8addd602ce36e4b409d64493c2e629
burstsquad <javanzz@163.com>
2020-04-14 15:13:48 +0800

    feat: http exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './common/pipes/validation.pipe';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
 
   // 校验
   app.useGlobalPipes(new ValidationPipe());
+
+  // 异常 filter
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
 }
